6041e4474b752a92b12e491ab87b963bd7bfba75
YakuzzUpsilon <57992666+YakuzzUpsilon@users.noreply.github.com>
2019-11-22 19:09:12 +0100

    Update Le-Royaume.js

--- .gitignore/Le-Royaume.js
+++ .gitignore/Le-Royaume.js
@@ -296,7 +296,12 @@ client.on('message', function (message) {
 })
 
 client.on("guildMemberAdd", member => {
-    member.guild.channels.get(process.env.GENERAL).send(`Bienvenue ${member} sur le serveur.`)
+    member.guild.channels.get("646375095883005987").send(`Salut ${member}, bienvenue dans le ${member.guild.name} :crown: !
+
+    :zap: N'oublie pas de lire le règlement: ${member.guild.channels.get(process.env.REGLEMENT)}
+    Ensuite pour ajouter tes rôles c'est ici: ${member.guild.channels.get(process.env.ROLE)} :zap:
+    N'hésites pas à inviter tes ami(e)s !
+    Tu es le ${member.guild.memberCount} membre du serveur!`)
 })
 
 client.on('message', function (message) {
