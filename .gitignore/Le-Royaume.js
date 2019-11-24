71247b7b4c38d0ac0a9182230d60d513a0645c32
YakuzzUpsilon <57992666+YakuzzUpsilon@users.noreply.github.com>
2019-11-24 14:16:31 +0100

    Update Le-Royaume.js

--- .gitignore/Le-Royaume.js
+++ .gitignore/Le-Royaume.js
@@ -294,15 +294,15 @@ client.on('message', function (message) {
         }, ms(bantime));
      }
 })
-client.on('guildMemberAdd', function (message) {
+client.on('guildMemberAdd', function (member) {
         let embed = new Discord.RichEmbed()
-        .setDescription(`Salut ${member}, bienvenue dans le ${message.guild.name} :smiling_imp: !
+        .setDescription(`Salut ${member}, bienvenue dans le ${member.guild.name} :smiling_imp: !
 
-        :boom: N'oublie pas de lire le règlement: ${message.guild.channels.get(process.env.REGLEMENT)}
-        :boom: Ensuite pour ajouter tes rôles c'est ici: ${message.guild.channels.get(process.env.ROLE)}
+        :boom: N'oublie pas de lire le règlement: ${member.guild.channels.get(process.env.REGLEMENT)}
+        :boom: Ensuite pour ajouter tes rôles c'est ici: ${member.guild.channels.get(process.env.ROLE)}
         :boom: N'hésites pas à inviter tes ami(e)s !`)
-        .setFooter(`Tu es le ${message.guild.memberCount}ème membre du serveur!`)
-        message.guild.channels.get(process.env.GENERAL).send(embed)
+        .setFooter(`Tu es le ${member.guild.memberCount}ème membre du serveur!`)
+        member.guild.channels.get(process.env.GENERAL).send(embed)
         }
 })
 
