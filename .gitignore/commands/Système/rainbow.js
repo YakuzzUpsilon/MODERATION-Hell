5f0452e158f132eea909391e580f9a5687718b8d
YakuzzUpsilon <57992666+YakuzzUpsilon@users.noreply.github.com>
2020-01-03 22:22:22 +0100

    Update rainbow.js

--- .gitignore/commands/Système/rainbow.js
+++ .gitignore/commands/Système/rainbow.js
@@ -8,7 +8,7 @@ class Rainbow extends Command {
       description: "Le role change de couleur.",
       usage: "rainbow",
       category: "Système",
-      permLevel: "🔑"
+      permLevel: "Co-Fondatrice"
     });
   }
 
