97f6ab93b1b06f52f7bfdd9dab77f4db1d743238
YakuzzUpsilon <57992666+YakuzzUpsilon@users.noreply.github.com>
2020-01-03 22:21:33 +0100

    Update kick.js

--- .gitignore/commands/Système/kick.js
+++ .gitignore/commands/Système/kick.js
@@ -8,7 +8,7 @@ class Kick extends Command {
       description: "Kick un utilisateur.",
       usage: "kick",
       category: "Système",
-      permLevel: "🔥"
+      permLevel: "Co-Fondatrice"
     });
   }
 
