efa67001b01c009f2f17c55a141f8b592d322195
YakuzzUpsilon <57992666+YakuzzUpsilon@users.noreply.github.com>
2020-01-08 00:32:18 +0100

    Update sanctions.js

--- .gitignore/commands/Système/sanctions.js
+++ .gitignore/commands/Système/sanctions.js
@@ -10,7 +10,7 @@ class Sanctions extends Command {
       description: "affiche les sanctions d'un membre.",
       usage: "sanctions",
       category: "Système",
-      permLevel: "Co-Fondatrice"
+      permLevel: "⚡"
     });
   }
 
